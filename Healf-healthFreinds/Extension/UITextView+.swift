2f049b45ebcc14e9511a9fd10f451f043bc0f91f
rrheon <yongheon0806@gmail.com>
2024-03-02 16:14:32 +0900

    feat - 운동 기록 작성 VC 구현, searchCell 수정

---
+++ Healf-healthFreinds/Extension/UITextView+.swift
@@ -0,0 +1,8 @@
+//
+//  UITextView+.swift
+//  Healf-healthFreinds
+//
+//  Created by 최용헌 on 3/2/24.
+//
+
+import Foundation
---
+++ Healf-healthFreinds/Main/Search/SearchResultCell.swift
@@ -0,0 +1,8 @@
+//
+//  SearchResultCell.swift
+//  Healf-healthFreinds
+//
+//  Created by 최용헌 on 3/2/24.
+//
+
+import Foundation
